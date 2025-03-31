bbedbc03c188c90abfe866318cd3e7f3e69a98c5
bity3pip <egor.sh23@gmail.com>
2025-03-31 21:31:19 +0200

    add middleware for CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import notes, analyse, gemini_end
 
@@ -6,6 +7,17 @@ from .routers import notes, analyse, gemini_end
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(notes.router, prefix="/notes", tags=["notes"])
 app.include_router(analyse.router, prefix="/analyses" ,tags=["analyses"])
 app.include_router(gemini_end.router, prefix="/gemini", tags=["gemini_end"])
